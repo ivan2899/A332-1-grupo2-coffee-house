8bc6eeba0206a4276594301315fa887672fe587d
Ivan <ivan.cordoba2002@gmail.com>
2024-07-09 03:21:13 -0300

    Arreglos luego de correccion

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-function showSidebar(){
+function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
-function hideSidebar(){
+function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+$('.modal').on('shown.bs.modal', function () {
+    $('body').addClass('no-scroll');
+});
+
+$('.modal').on('hidden.bs.modal', function () {
+    $('body').removeClass('no-scroll');
+});
