43c87ef366a70cb7847c03b97ed1929222ff0b85
SebastianSerranoB <sebastianserranob11@gmail.com>
2024-07-12 14:51:30 -0300

    busqueda y filtrado de productos funciona

--- js/filterProducts.js
+++ js/filterProducts.js
@@ -1,10 +1,27 @@
 
-function filterProducts() {
+function filterProducts() 
+{
     const priceRange = document.getElementById('priceRange').value;
     const productName = document.getElementById('productName').value.toLowerCase();
     const productType = document.getElementById('productType').value;
   
     const products = document.querySelectorAll('.product-card');
+  
+    
+    let activeFilter = null;
+    
+    if (productType && productType !== "Seleccione el tipo") {
+      activeFilter = 'type';
+    } else if (productName) {
+      activeFilter = 'name';
+    } else if (priceRange  && priceRange !== "Seleccione un rango") {
+      activeFilter = 'price';
+    } else {
+      activeFilter = 'type';
+    return;
+    }
+   
+   
   
     //callback fn, valida si coincide values de prod y desplega la card correspondiente.
     products.forEach(product => {
@@ -12,45 +29,73 @@ function filterProducts() {
       const productNameData = product.getAttribute('data-name').toLowerCase();
       const productTypeData = product.getAttribute('data-type');
   
-      let priceMatch = false;
-      let nameMatch = false;
-      let typeMatch = false;
+      let match = false;
   
-      if (priceRange) {
-        const [minPrice, maxPrice] = priceRange.split('-');
-        priceMatch = productPrice >= parseInt(minPrice) && (maxPrice ? productPrice <= parseInt(maxPrice) : true);
-      } else {
-        priceMatch = true;
-      }
-  
-      if (productName) {
-        nameMatch = productNameData.includes(productName);
-      } else {
-        nameMatch = true;
-      }
-  
-      if (productType) {
-        typeMatch = productType === productTypeData;
-      } else {
-        typeMatch = true;
-      }
+     
+      if (activeFilter === 'type') 
+      {
+        match = productType === productTypeData;
+        document.getElementById('priceRange').selectedIndex = 0;
+        document.getElementById('productName').value = '';
+      } 
+      else if (activeFilter === 'price') 
+      {
+        if (priceRange === "500-10000") 
+        {
+         match = productPrice > 500;
+        }
+        else
+        {
+          let [minPrice, maxPrice] = priceRange.split('-');
+          console.log(`min price: ${minPrice}, max price: ${maxPrice}`);
+       
+          if( parseInt(productPrice) >= parseInt(minPrice) && parseInt(productPrice) <= parseInt(maxPrice))
+          {
+            match = true;
+          }
+        }
 
-      // debuggear
-      console.log(`productName: ${productNameData}, ProductPrice: ${productPrice}, productTypeData: ${productTypeData}, Price Match: ${priceMatch}, Name Match: ${nameMatch}, Type Match: ${typeMatch}`);
+      }
+      else if (activeFilter === 'name') 
+      {
+        if(productNameData.includes(productName))
+        {
+          match = true;
+        }
+        document.getElementById('priceRange').selectedIndex = 0;
+        document.getElementById('productType').selectedIndex = 0;
+      }
+      
 
-      if (/*priceMatch || nameMatch ||*/ typeMatch) {
+      if(match == true)
+      {
         product.style.display = '';
-      } else {
+      }
+      else
+      {
         product.style.display = 'none';
       }
+  
+    
     });
   }
   
+
+
+
+
+
+
   function resetFilters() {
+    
     const products = document.querySelectorAll('.product-card');
     products.forEach(product => {
       product.style.display = '';
     });
+
+    document.getElementById('priceRange').selectedIndex = 0;
+    document.getElementById('productName').value = "";
+    document.getElementById('productType').selectedIndex = 0;
   }
   
 
